2425d77cd34a8dd5a5dff4b5fd7bc1f32dce9d81
Sudharsan2710 <155613808+Sudharsan2710@users.noreply.github.com>
2024-04-12 17:06:26 +0530

    PatternB.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,41 +1,46 @@
 package com.chainsys.jfs3;
-
-public class Pattern2 {
+import java.util.Scanner;
+public class Patternb {
 
 	public static void main(String[] args) {
-				
-				 int n = 6;
-			        
-			        // Upper half
-			        for (int i = 0; i < n; i++) {
-			            // Print spaces before the stars
-			            for (int j = i; j < n - 1; j++) {
-			                System.out.print(" ");
-			            }
-			            // Print stars
-			            for (int j = 0; j < 2 * i + 1; j++) {
-			                System.out.print("*");
-			            }
-			            // Move to the next line
-			            System.out.println();
-			        }
-			        
-			        // Lower half
-			        for (int i = n - 2; i >= 0; i--) {
-			            // Print spaces before the stars
-			            for (int j = i; j < n - 1; j++) {
-			                System.out.print(" ");
-			            }
-			            // Print stars
-			            for (int j = 0; j < 2 * i + 1; j++) {
-			                System.out.print("*");
-			            }
-			            // Move to the next line
-			            System.out.println();
-		        }
-		    }
-		
-
+		Scanner sc = new Scanner(System.in);
+		  System.out.println("Enter The Number : ");
+	        int Number = sc.nextInt();
+	        
+	        do {
+	            if (Number < 0) {
+	                System.out.println("Please enter a positive number.");
+	                Number = sc.nextInt();
+	            }
+	        } while (Number < 0);
+	        
+	       
+	
+	            for(int i=1; i<Number; i+=2)
+	            {
+	                for(int j=i; j<=Number; j+=1)
+	                {
+	                    System.out.print(" ");
+	                }
+	                for(int j=1; j<=i; j+=1)
+	                {
+	                    System.out.print("*");
+	                }
+	                System.out.println();
+	            }
+	            for(int i=1; i<=Number; i+=2)
+	            {
+	                for(int j=1; j<=i; j+=1)
+	                {
+	                    System.out.print(" ");
+	                }
+	                for(int j=i; j<=Number; j+=1)
+	                {
+	                    System.out.print("*");
+	                }
+	                System.out.println();
+	            }
+	        }
 	}
 
 
